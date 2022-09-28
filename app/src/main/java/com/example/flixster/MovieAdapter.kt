60f38ae2401b47332d574dd79867c96fcc62908f
joeporada <poradajoe@gmail.com>
2022-09-28 02:07:29 -0400

    flix part2

--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>, private val ori: String)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
@@ -25,11 +27,15 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
     override fun getItemCount() = movies.size
 
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
 
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
 
         fun bind(movie: Movie){
             tvTitle.text = movie.title
@@ -38,6 +44,13 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
                 Glide.with(context).load(movie.posterImageUrl).placeholder(R.drawable.loading).into(ivPoster)}
             else if(ori == "land"){
                 Glide.with(context).load(movie.backdropImageUrl).placeholder(R.drawable.loading).into(ivPoster)}
+        }
+
+        override fun onClick(v: View?) {
+            val movie = movies[adapterPosition]
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
         }
     }
 
